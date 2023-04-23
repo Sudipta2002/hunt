3a64181d630c786906e33d4df6564bd82cfa85cc
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-04-23 18:29:57 +0530

    updtd

--- backend/index.js
+++ backend/index.js
@@ -26,17 +26,12 @@ app.use('/api/v1/signIn', (req, res) => {
     return res.json({ message: "Hitting the signIn service" });
 });
 
-app.get('/', (req, res) => {
-    // res.send("Apprunning Succeessfully");
 
-});
 if (process.env.NODE_ENV == 'production') {
     const path = require('path');
-    // const __dirname1 = path.resolve();
+
     app.get('/', (req, res) => {
         res.send("Apprunning Succeessfully");
-        app.use(express.static(__dirname, '../frontend', 'build'));
-        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
     })
 }
 
