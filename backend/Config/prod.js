44ea53fa3dfc93b53955808233bf7cd303f21aa4
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-04-23 20:45:21 +0530

    ss

--- backend/Config/prod.js
+++ backend/Config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 'https: //hunt-backend.vercel.app/',
+    PORT: 'https: //hunt-backend.vercel.app',
     SECRET_OR_KEY: process.env.SECRET_OR_KEY,
     MONGO_URI: process.env.MONGO_URI
 }
